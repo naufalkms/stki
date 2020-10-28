2bb6dfc564aa89d1f27f433b63a79175b1a9dbeb
naufalkms <kmsmuhammadnaufal@gmail.com>
2020-10-28 17:15:57 +0700

    update file stki

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ h2 {
 <a href="tf.php">Term Frequency</a> |
 <a href="idf.php">Invers Document Frequency</a> |
 <a href="tfidf.php">TF.IDF</a> |
+<a href="search1.php">Search</a> |
 </div>
 <hr/>
 </body>
